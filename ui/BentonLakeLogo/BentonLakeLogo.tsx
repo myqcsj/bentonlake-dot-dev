cd0a4e3a6af428ea7533c82a3a6f2daec6f2d42b
Benton Lake <benton@bentonlake.dev>
2023-03-16 18:18:41 -0400

    Navbar prettied

--- ui/BentonLakeLogo/BentonLakeLogo.tsx
+++ ui/BentonLakeLogo/BentonLakeLogo.tsx
@@ -2,17 +2,15 @@ import { major_mono_display } from '@/lib/fonts';
 
 export default function BentonLakeLogo() {
   return (
-    <div className="flex items-center text-white">
-      <p className={`${major_mono_display.className} text-2xl text-violet-800`}>
-        {'<'}
-      </p>
-      <p className={`${major_mono_display.className} text-4xl`}>
+    <div
+      className={`flex items-center text-white ${major_mono_display.className}`}
+    >
+      <p className="text-2xl text-violet-800">{'<'}</p>
+      <p className="text-4xl">
         <span className="font-semibold">{'BeNToNLAKe'}</span>
         <span className="font-thin">{'.dev'}</span>
       </p>
-      <p className={`${major_mono_display.className} text-2xl text-violet-800`}>
-        {'/>'}
-      </p>
+      <p className="text-2xl text-violet-800">{'/>'}</p>
     </div>
   );
 }
--- ui/NavItem/NavItem.tsx
+++ ui/NavItem/NavItem.tsx
@@ -11,20 +11,48 @@ export default function NavItem({
   text: string;
   href: string;
 }) {
+  const outerTag = {
+    hovered: { opacity: 1, scale: 1.02 },
+  };
+  const innerTag = {
+    hovered: { color: '#5b21b6' },
+  };
+
   return (
     <Link href={href}>
       <motion.div
         className="font-semibold flex items-center w-fit gap-1"
         initial={false}
         animate={{ opacity: 0.6 }}
-        whileHover={{
-          opacity: 1,
-        }}
-        transition={{ duration: 0.4, ease: 'easeInOut' }}
+        whileHover="hovered"
+        variants={outerTag}
+        transition={{ duration: 0.15, ease: 'easeInOut' }}
       >
-        <span className="text-gray-500 text-lg">&lt;</span>
-        <span className="text-gray-100 text-2xl">{text}</span>
-        <span className="text-gray-500 text-lg">/&gt;</span>
+        <motion.span
+          className="text-lg"
+          initial={false}
+          animate={{ color: '#6b7280' }}
+          variants={innerTag}
+          transition={{ duration: 0.15, ease: 'easeInOut' }}
+        >
+          &lt;
+        </motion.span>
+        <motion.span
+          className="text-2xl"
+          initial={false}
+          animate={{ color: '#f3f4f6' }}
+        >
+          {text}
+        </motion.span>
+        <motion.span
+          className="text-lg"
+          initial={false}
+          animate={{ color: '#6b7280' }}
+          variants={innerTag}
+          transition={{ duration: 0.15, ease: 'easeInOut' }}
+        >
+          /&gt;
+        </motion.span>
       </motion.div>
     </Link>
   );
